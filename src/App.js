626b555e1f9b52b7295c158525dd9af6e55cfedb
Gabriela <gabriela.oz@yahoo.com>
2023-06-25 14:03:48 +0300

    Included thr link to my GitHub repository at the bottom of the project page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <h1>Wheather App</h1>
       <WeatherSearch />
+      <p>
+        This project was coded by Gabriela Lefter an it is
+        <a
+          href="https://github.com/gabrielalowe/weather-react"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          open-sourced
+        </a>
+      </p>
     </div>
   );
 }
